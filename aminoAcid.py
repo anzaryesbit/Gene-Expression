db035b2cd406235424aeb1a38258862814c296c6
Aiyana <aarnobit19@student.sacredhearts.org>
2018-12-20 07:40:38 -1000

    Update aminoAcid.py

--- aminoAcid.py
+++ aminoAcid.py
@@ -1,11 +1,3 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
-
 def aminoacid(a, b, c):
     dna_sequence = a + b + c
     print('DNA Sequence: ' + dna_sequence)
